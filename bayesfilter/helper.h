d0f051208f3a48db864b9de2c7cd6ad694133bdc
Zhengnan Chen <64331738+chenzhengnan328@users.noreply.github.com>
2021-02-22 19:40:24 -0500

    Add files via upload

---
+++ bayesfilter/helper.h
@@ -0,0 +1,49 @@
+#ifndef HELP_FUNCTIONS_H_
+#define HELP_FUNCTIONS_H_
+
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+class Helpers {
+public:
+
+	//definition of one over square root of 2*pi:
+	constexpr static float STATIC_ONE_OVER_SQRT_2PI = 1/sqrt(2*M_PI) ;
+	float ONE_OVER_SQRT_2PI = 1/sqrt(2*M_PI) ;
+	static float normpdf(float x, float mu, float std) {
+	    return (STATIC_ONE_OVER_SQRT_2PI/std)*exp(-0.5*pow((x-mu)/std,2));
+	}
+
+	//static function to normalize a vector:
+	static std::vector<float> normalize_vector(std::vector<float> inputVector){
+
+		//declare sum:
+		float sum = 0.0f;
+
+		//declare and resize output vector:
+		std::vector<float> outputVector ;
+		outputVector.resize(inputVector.size());
+
+		//estimate the sum:
+		for (unsigned int i = 0; i < inputVector.size(); ++i) {
+			sum += inputVector[i];
+		}
+
+		//normalize with sum:
+		for (unsigned int i = 0; i < inputVector.size(); ++i) {
+			outputVector[i] = inputVector[i]/sum;
+		}
+
+		//return normalized vector:
+		return outputVector;
+	}
+	
+};
+
+#endif /* HELP_FUNCTIONS_H_ */
